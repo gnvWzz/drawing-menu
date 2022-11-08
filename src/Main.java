522547f02e3162918771ab2603ed0d22faf12546
gnvWzz <quangvuongdac2111@gmail.com>
2022-11-08 09:05:55 +0700

    123

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,43 @@ public class Main {
         System.out.println("Enter your choice: ");
         int choice = sc.nextInt();
 
-        switch (choice) {
-            case 0:
-                System.out.println("No choice!");
-                break;
-            case 1:
-                System.out.println("Enter the triangle length:");
-                int triLength = sc.nextInt();
-                for (int i = 1; i <= triLength; i++) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print("*");
+        while (choice != 0) {
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the triangle length:");
+                    int triLength = sc.nextInt();
+                    for (int i = 1; i <= triLength; i++) {
+                        for (int j = 1; j <= i; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
-                }
-                break;
-            case 2:
-                System.out.println("Enter the square length:");
-                int squareLength = sc.nextInt();
-                for (int i = 1; i <= squareLength; i++) {
-                    for (int j = 1; j <= squareLength * 2; j++) {
-                        System.out.print("*");
+                    break;
+                case 2:
+                    System.out.println("Enter the square length:");
+                    int squareLength = sc.nextInt();
+                    for (int i = 1; i <= squareLength; i++) {
+                        for (int j = 1; j <= squareLength * 2; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
-                }
-                break;
-            case 3:
-                System.out.println("Enter the rectangle height:");
-                int recHeight = sc.nextInt();
-                System.out.println("Enter the rectangle width:");
-                int recWidth = sc.nextInt();
-                for (int i = 1; i <= recHeight; i++) {
-                    for (int j = 1; j <= recWidth; j++) {
-                        System.out.print("*");
+                    break;
+                case 3:
+                    System.out.println("Enter the rectangle height:");
+                    int recHeight = sc.nextInt();
+                    System.out.println("Enter the rectangle width:");
+                    int recWidth = sc.nextInt();
+                    for (int i = 1; i <= recHeight; i++) {
+                        for (int j = 1; j <= recWidth; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
                     }
-                    System.out.println();
-                }
-                break;
+                    break;
+            }
+
         }
+        System.out.println("Hello world");
     }
 }
